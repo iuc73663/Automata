ceacf05de35e4e598ee4e9981be8abbd552193c8
iuc73663 <ivan5247@gmail.com>
2019-09-01 22:41:57 -0700

    last push of the day

--- Math Adventures with Python/Testing.py
+++ Math Adventures with Python/Testing.py
@@ -8,62 +8,6 @@ Created on Mon Aug 26 16:32:46 2019
 import tkinter as tk
 from tkinter import ttk
 from turtle import RawTurtle, TurtleScreen, ScrolledCanvas 
-        
-class Adder(ttk.Frame):
-    """The adders gui and functions."""
-    def __init__(self, parent, *args, **kwargs):
-        ttk.Frame.__init__(self, parent, *args, **kwargs)
-        self.root = parent
-        self.init_gui()
-        
-    def on_quit(self):
-        """Exits program."""
-        quit()
-        
-    def calculate(self):
-        if not(self.shapeSelect.get() == "") and not(self.step_entry.get() == ""):
-            window = tk.Tk()
-            canvas = ScrolledCanvas(master=window, width=800, height=600)
-            canvas.pack(fill=tk.BOTH, expand=tk.YES)
-            screen = TurtleScreen(canvas)
-            turtle = RawTurtle(screen)
-            cWidth = 800
-            cHeight = 600
-            x = 300
-            y = 100
-            window.geometry('%dx%d+%d+%d' % (cWidth, cHeight, x, y))
-            driver(self.shapeSelect.get(), int(self.step_entry.get()), turtle)                    
-    def init_gui(self):
-        """Builds GUI."""
-        self.root.title('Fractal Maker')
-        self.grid(column=0, row=0, sticky='nsew')
-        
-        # get screen width and height
-        #screen_width = root.winfo_screenwidth()
-        #screen_height = root.winfo_screenheight()
-        rootWidth = 250
-        rootHeight = 150
-        x = 0
-        y = 0
-        root.geometry('%dx%d+%d+%d' % (rootWidth, rootHeight, x, y))        
-        self.root.geometry()
-
-        ttk.Label(self, text='Shape').grid(column=0, row=2, sticky='w')
-        self.shapeSelect = ttk.Combobox(self, values=["square", "triangle", "star"])
-        self.shapeSelect.grid(column=1, row = 2)
-        
-        ttk.Label(self, text='Depth').grid(column=0, row=3, sticky='w')
-        self.step_entry = ttk.Entry(self, width=6)
-        self.step_entry.grid(column=1, row = 3)
-        
-        self.calc_button = ttk.Button(self, text='Calculate', command=self.calculate)
-        self.calc_button.grid(column=0, row=4, columnspan=4)
-        
-
-        ttk.Separator(self, orient='horizontal').grid(column=0, row=1, columnspan=4, sticky='ew')
-
-        for child in self.winfo_children():
-            child.grid_configure(padx=10, pady=10)
 
 class TurtleCanvas:
     window = tk.Tk()
@@ -111,11 +55,62 @@ class TurtleCanvas:
             self.square(20 + i)
             self.turtle.right(2)
             i += 1 
-#if __name__ == '__main__':
-#    root = tk.Tk()
-#    Adder(root)
-#    root.mainloop()
+            
+class Adder(ttk.Frame):
+    """The adders gui and functions."""
+    def __init__(self, parent, *args, **kwargs):
+        ttk.Frame.__init__(self, parent, *args, **kwargs)
+        self.root = parent
+        self.init_gui()
+        
+    def on_quit(self):
+        """Exits program."""
+        quit()
+        
+    def calculate(self):
+        if not(self.shapeSelect.get() == "") and not(self.step_entry.get() == ""):
+            sample = TurtleCanvas(100,250)
+            sample.squareDriver(int(self.step_entry.get()))     
+            #sample.window.mainloop()             
+    def init_gui(self):
+        """Builds GUI."""
+        self.root.title('Fractal Maker')
+        self.grid(column=0, row=0, sticky='nsew')
+        
+        # get screen width and height
+        #screen_width = root.winfo_screenwidth()
+        #screen_height = root.winfo_screenheight()
+        rootWidth = 250
+        rootHeight = 150
+        x = 0
+        y = 0
+        root.geometry('%dx%d+%d+%d' % (rootWidth, rootHeight, x, y))        
+        self.root.geometry()
 
-sample = TurtleCanvas(200,300)
-sample.squareDriver(30)
-sample.window.mainloop()
+        ttk.Label(self, text='Shape').grid(column=0, row=2, sticky='w')
+        self.shapeSelect = ttk.Combobox(self, values=["square", "triangle", "star"])
+        self.shapeSelect.grid(column=1, row = 2)
+        
+        ttk.Label(self, text='Depth').grid(column=0, row=3, sticky='w')
+        self.step_entry = ttk.Entry(self, width=6)
+        self.step_entry.grid(column=1, row = 3)
+        
+        self.calc_button = ttk.Button(self, text='Calculate', command=self.calculate)
+        self.calc_button.grid(column=0, row=4, columnspan=4)
+        
+
+        ttk.Separator(self, orient='horizontal').grid(column=0, row=1, columnspan=4, sticky='ew')
+
+        for child in self.winfo_children():
+            child.grid_configure(padx=10, pady=10)
+
+
+            
+if __name__ == '__main__':
+    root = tk.Tk()
+    Adder(root)
+    root.mainloop()
+
+#sample = TurtleCanvas(200,300)
+#sample.squareDriver(30)
+#sample.window.mainloop()
